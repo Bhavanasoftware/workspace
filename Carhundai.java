149609a24d832477a6cdee7f5bf4b97dc3abb762
BHAVANA <bhavanaroyalp@gmail.com>
2023-04-18 16:08:07 +0530

    first commit

---
+++ Carhundai.java
@@ -0,0 +1,14 @@
+package firstproject;
+
+public class Carhundai {
+     public static void main(String[] args) {
+    	 int i=5;
+         int k=9;
+              int add =i+k; 
+              
+            System.out.println( add);
+
+     }  
+	
+            
+}
---
+++ Carsuzuki.java
@@ -0,0 +1,13 @@
+package firstproject;
+
+public class Carsuzuki {
+    public static void main(String[] args) {
+		int a=23;
+		int x=77;
+		int p=90;
+		
+		int pcrt =a*x/p;
+		
+		System.out.println(pcrt);
+	}
+}
